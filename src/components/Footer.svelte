344fb8ab640c012c0658d7e48034d767dff4f054
Fynn Budde <FoibLP@gmail.com>
2025-03-21 16:55:54 +0100

    Update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="px-[20%] pb-8 pt-32 bg-league-hextech-black font-spiegel text-center">
-	<p class="mb-4 text-league-grey-2">© 2023 PENTAKILL.LOL</p>
+	<p class="mb-4 text-league-grey-2">© 2025 PENTAKILL.LOL</p>
 	<p class="text-xs text-league-grey-2">
 		PENTAKILL.LOL isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot
 		Games or anyone officially involved in producing or managing League of Legends. League of
